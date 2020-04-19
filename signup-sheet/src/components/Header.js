860f578202bda3d621c9ca0968fe44c8ca01639d
Sam <sam@subsupps.co.nz>
2020-04-19 16:18:46 +1200

    Initial commit

---
+++ signup-sheet/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { AppBar } from '@material-ui/core';
+
+function Header(){
+    return(
+        <div>
+            <AppBar position="static">
+               
+            </AppBar>
+        </div>
+    )
+}
+
+export default Header;
