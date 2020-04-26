49d0d237e90552fe3dc28c7bb61675b08a798f26
Sam <magnum.et@gmail.com>
2020-04-26 16:17:13 +1200

    Completes styling for the registrations container

--- signup-sheet/src/components/RegoContainer.js
+++ signup-sheet/src/components/RegoContainer.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles, createMuiTheme } from '@material-ui/core/styles';
+import ArchiveIcon from '@material-ui/icons/Archive';
+import Divider from '@material-ui/core/Divider';
 
 const theme = createMuiTheme();
 
@@ -16,6 +18,14 @@ const useStyles = makeStyles(() => ({
       marginRight: theme.spacing(2),
       marginLeft: theme.spacing(2),
     },
+    title: {
+        paddingBottom: '10px',
+        verticalAlign: 'bottom'
+    },
+    icon: {
+        display: 'inline-flex',
+        verticalAlign: '-6px' 
+    }
   }));
 
   function RegoContainer(){
@@ -26,6 +36,10 @@ const useStyles = makeStyles(() => ({
             <Grid container spacing={3}>
                 <Grid item xs={12}>
                     <Paper className={classes.paper}>
+                        <div className={classes.title}> 
+                            <span> <ArchiveIcon fontSize="large" color="primary" elementType="span" className ={classes.icon}/> Registrations</span>
+                        </div>
+                        <Divider />
                     </Paper>
                 </Grid>
             </Grid>
